001455d61fca767b47e16eafbadca29653189708
uriel71 <124410672+uriel71@users.noreply.github.com>
2026-06-19 13:46:59 -0700

    Add files via upload

---
+++ Backend/crear.sql
@@ -0,0 +1,27 @@
+CREATE TABLE tareas(
+
+id INTEGER PRIMARY KEY,
+titulo TEXT,
+url TEXT
+
+);
+
+
+INSERT INTO tareas VALUES
+(
+1,
+'¿Cuál es la relación entre métodos HTTP y MySQL con Node.js? ¿Qué es un cURL?',
+'https://lh3.googleusercontent.com/drive-storage/AJQWtBNypEHKmIXtYw_LvCp02oFb4Koat97Fb7EZ_ZUq_ryVdq8ZG95NpKKAdUdUy5ZrMQ7GdIhwDFLgt-RMS54cJ66FhwLNOBeHSC-kiaJQwAOp_qLBlQ=w1920-h868?auditContext=forDisplay'
+),
+
+(
+2,
+'Juego de dados',
+'https://docs.google.com/document/d/1UmsnfoMS_TL9cCBEFJoaW8IF0uPaKIdJ/edit'
+),
+
+(
+3,
+'Tablas de multiplicar',
+'https://docs.google.com/document/d/1t1laBBUm6gkavwYlks4aIQFdOsxdlFeMAsZSGOSaI1s/edit?tab=t.0'
+);
